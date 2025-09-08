b9bd702e058a6f90ed1d0da9a8fc9ad7efff3eb6
Bagas Syaddad C <bagassyaddad98@gmail.com>
2025-09-08 19:28:14 +0700

    Commit No. 2 – add coffee list & detail fragment

--- app/src/main/java/com/example/lab_week_3/MainActivity.kt
+++ app/src/main/java/com/example/lab_week_3/MainActivity.kt
@@ -1,37 +1,22 @@
 package com.example.lab_week_3
 
 import android.os.Bundle
-import android.util.Log
-import android.view.View
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
-    companion object { private const val TAG = "MainActivity" }
+// Interface listener untuk komunikasi antar fragment
+interface CoffeeListener {
+    fun onSelected(id: Int)
+}
 
+class MainActivity : AppCompatActivity(), CoffeeListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main_fragment)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
-
-        Log.d(TAG, "onCreate")
     }
 
-    override fun onStart() { super.onStart(); Log.d(TAG, "onStart") }
-    override fun onResume() { super.onResume(); Log.d(TAG, "onResume") }
-    override fun onPause() { super.onPause(); Log.d(TAG, "onPause") }
-    override fun onStop() { super.onStop(); Log.d(TAG, "onStop") }
-    override fun onDestroy() { super.onDestroy(); Log.d(TAG, "onDestroy") }
-
-    private fun enableEdgeToEdge() {
-        window.decorView.systemUiVisibility =
-            View.SYSTEM_UI_FLAG_LAYOUT_STABLE or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+    override fun onSelected(id: Int) {
+        val detailFragment = supportFragmentManager
+            .findFragmentById(R.id.fragment_detail) as DetailFragment
+        detailFragment.setCoffeeData(id)
     }
 }
